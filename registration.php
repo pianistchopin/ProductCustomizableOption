d45f277f868400fdec7a281710e2fcc5d374b4d1
Thomas <pianist.chopin.1@gmail.com>
2021-11-25 14:17:32 +0000

    init project

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'TenxEngineer_ProductCustomizableOption',
+    __DIR__
+);
